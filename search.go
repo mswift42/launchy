f98260d827d212562b97b5660913cd5896563fb6
mswift42 <martin.haesler@gmail.com>
2016-06-12 12:58:45 +0100

    Add locate Command

--- search.go
+++ search.go
@@ -1,5 +1,7 @@
 package main
 
+import "os/exec"
+
 // Searchresult represents the result of a local search.
 type Searchresult struct {
 	name      string
@@ -11,7 +13,8 @@ type Searchresult struct {
 // Videos, Documents, Pictures, Downloads, ...
 type Bookmarks []string
 
-var bookmarks = Bookmarks{"", "/Documents", "/.Downloads", "/Music", "Pictures", "/Videos"}
+var bookmarks = Bookmarks{"", "/Documents", "/.Downloads",
+	"/Music", "Pictures", "/Videos"}
 
 // SearchresultNames takes a slice of Searchresults and
 // returns of slice of its names.
@@ -21,6 +24,13 @@ func SearchresultNames(sr []Searchresult) []string {
 		results[i] = sr[i].name
 	}
 	return results
+}
+
+// locateCommand takes a queryvalue ant returns an exec.Command
+// with the 'locate' command, ignoring case, limited to 20 queries,
+// matching only the 'base name', searching for the query value.
+func locateCommand(query string) *exec.Cmd {
+	return exec.Command("locate", "-l", "20", "-b", "-i", query)
 }
 func main() {
 

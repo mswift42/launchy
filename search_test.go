b05da53e28789e5a8b9ff0d8e59dbbe7b8bbdbaa
mswift42 <martin.haesler@gmail.com>
2016-06-13 13:22:03 +0100

    test findCommands

--- search_test.go
+++ search_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os/user"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,4 +22,26 @@ func TestLocateCommand(t *testing.T) {
 	assert := assert.New(t)
 	l1 := locateCommand("go")
 	assert.Equal(l1.Path, "/usr/bin/locate")
+	assert.Equal(l1.Args, []string{"locate", "-l", "20", "-b", "-i", "go"})
+	assert.Equal(l1.Env, []string(nil))
+}
+
+func TestFindCommandBookmarks(t *testing.T) {
+	assert := assert.New(t)
+	usr, err := user.Current()
+	assert.Equal(err, nil)
+	f1, err := findCommandBookmarks("/Documents", "go")
+	assert.Equal(err, nil)
+	assert.Equal(f1.Path, "/usr/bin/find")
+	assert.Equal(f1.Env, []string(nil))
+	assert.Equal(f1.Args[1:], []string{usr.HomeDir + "/Documents", "-maxdepth", "2",
+		"-iname", "*go*"})
+}
+
+func TestFindCommandBinaries(t *testing.T) {
+	assert := assert.New(t)
+	f1 := findCommandBinries("/opt", "go")
+	assert.Equal(f1.Path, "/usr/bin/find")
+	assert.Equal(f1.Args[1:], []string{"/opt", "-maxdepth", "2", "-iname", "*go*"})
+	assert.Equal(f1.Env, []string(nil))
 }

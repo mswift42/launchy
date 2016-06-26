4fcafd9dac734cb90171bc53c361acf1084a2fee
mswift42 <martin.haesler@gmail.com>
2016-06-26 10:43:40 +0100

    add setup_buttonWithLabel

--- gui.go
+++ gui.go
@@ -42,7 +42,15 @@ func setup_scrolledWindow(hadjust, vadjust *gtk.Adjustment) *gtk.ScrolledWindow 
 func setup_label(text string) *gtk.Label {
 	label, err := gtk.LabelNew(text)
 	if err != nil {
-		log.Fatal("unable to setup label")
+		log.Fatal("unable to setup label: ", err)
 	}
 	return label
+}
+
+func setup_buttonWithLabel(text string) *gtk.Button {
+	btn, err := gtk.ButtonNewWithLabel(text)
+	if err != nil {
+		log.Fatal("unable to setup button: ", err)
+	}
+	return btn
 }
